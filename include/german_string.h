9fe7f6b06670629cb89407340c4d04f905831e40
Dmytro WSL <dmytroshynkar@gmail.com>
2025-04-28 08:44:09 +0200

    some basic tests

--- include/german_string.h
+++ include/german_string.h
@@ -4,6 +4,7 @@
 #include <cstdint>
 #include <cstring>
 #include <cassert>
+#include <compare> 
 
 namespace gs
 {
@@ -17,7 +18,8 @@ namespace gs
     {
     };
 
-    class german_string
+    template<typename TAllocator = std::allocator<char>>
+    class basic_german_string
     {
     public:
         using size_type = std::uint32_t;
@@ -32,134 +34,182 @@ namespace gs
         static constexpr std::uint64_t PTR_TAG_MASK = static_cast<std::uint64_t>(0b11) << 62;
 
     private:
-        // The first 4 bytes are the size of the string
-        // The next 4 bytes are the prefix
-        // The next 8 bytes are a tagged pointer, first 2 bits are the tag for class
-        std::uint64_t _state[2];
 
-        size_type _get_size() const
+        struct _gs_impl : TAllocator
         {
-            // relies on little endian
-            return static_cast<size_type>(_state[0]);
-        }
+            _gs_impl(TAllocator alloc) 
+                : TAllocator(alloc) 
+                , _state{0, 0}
+            {}
 
-        bool _is_small() const
-        {
-            return _get_size() <= SMALL_STRING_SIZE;
-        }
+            _gs_impl(const char *str, size_type size, string_class cls, const TAllocator& allocator)
+                : TAllocator(allocator)
+            {
+                _state[0] = size;
+                if (_is_small())
+                {
+                    std::memcpy(_get_maybe_small_ptr(), str, size);
+                }
+                else
+                {
+                    if (cls == string_class::temporary)
+                    {
+                        char *copied_str = static_cast<char*>(std::allocator_traits<TAllocator>::allocate(*this, size));
+                        if (!copied_str)
+                        {
+                            throw std::bad_alloc();
+                        }
 
-        const char *_get_ptr() const
-        {
-            // relies on the ptr being in user-space canonical form
-            // and the first 2 bits of the pointer being 0
-            return reinterpret_cast<const char *>(_state[1] & ~PTR_TAG_MASK);
-        }
+                        std::memcpy(copied_str, str, size);
+                        str = copied_str;
+                    }
+                    _state[1] = reinterpret_cast<std::uint64_t>(str) | _get_ptr_tag(cls);
+                }
+            }
 
-        char *_get_ptr()
-        {
-            // relies on the ptr being in user-space canonical form
-            // and the first 2 bits of the pointer being 0
-            return reinterpret_cast<char *>(_state[1] & ~PTR_TAG_MASK);
-        }
+            ~_gs_impl()
+            {
+                if (!_is_small())
+                {
+                    std::uint64_t tag = _state[1] & PTR_TAG_MASK;
+                    if (tag == _get_ptr_tag(string_class::temporary))
+                    {
+                        std::allocator_traits<TAllocator>::deallocate(*this, _get_ptr(), _get_size());
+                    }
+                }
+            }
 
-        char *_get_maybe_small_ptr()
-        {
-            return _is_small()
-                       ? (reinterpret_cast<char *>(&_state) + sizeof(size_type))
-                       : _get_ptr();
-        }
+            size_type _get_size() const
+            {
+                // relies on little endian
+                return static_cast<size_type>(_state[0]);
+            }
 
-        const char *_get_maybe_small_ptr() const
-        {
-            return _is_small()
-                       ? (reinterpret_cast<const char *>(&_state) + sizeof(size_type))
-                       : _get_ptr();
-        }
+            bool _is_small() const
+            {
+                return _get_size() <= SMALL_STRING_SIZE;
+            }
 
-        static constexpr std::uint64_t _get_ptr_tag(string_class cls)
-        {
-            return static_cast<std::uint64_t>(cls) << 62;
-        }
+            const char *_get_ptr() const
+            {
+                // relies on the ptr being in user-space canonical form
+                // and the first 2 bits of the pointer being 0
+                return reinterpret_cast<const char *>(_state[1] & ~PTR_TAG_MASK);
+            }
+
+            char *_get_ptr()
+            {
+                // relies on the ptr being in user-space canonical form
+                // and the first 2 bits of the pointer being 0
+                return reinterpret_cast<char *>(_state[1] & ~PTR_TAG_MASK);
+            }
+
+            char *_get_maybe_small_ptr()
+            {
+                return _is_small()
+                        ? (reinterpret_cast<char *>(&_state) + sizeof(size_type))
+                        : _get_ptr();
+            }
+
+            const char *_get_maybe_small_ptr() const
+            {
+                return _is_small()
+                        ? (reinterpret_cast<const char *>(&_state) + sizeof(size_type))
+                        : _get_ptr();
+            }
+
+            static constexpr std::uint64_t _get_ptr_tag(string_class cls)
+            {
+                return static_cast<std::uint64_t>(cls) << 62;
+            }
+
+            string_class _get_class() const
+            {
+                if (_is_small())
+                {
+                    return string_class::persistent;
+                }
+                else
+                {
+                    std::uint64_t tag = (_state[1] & PTR_TAG_MASK) >> 62;
+                    return static_cast<string_class>(tag);
+                }
+            }
+
+            bool _equals(const _gs_impl &other) const
+            {
+                if (_state[0] != other._state[0])
+                {
+                    return false;
+                }
+
+                if (_is_small())
+                {
+                    return _state[1] == other._state[1];
+                }
+                return std::memcmp(_get_ptr(), other._get_ptr(), _get_size()) == 0;
+            }
+
+            // The first 4 bytes are the size of the string
+            // The next 4 bytes are the prefix
+            // The next 8 bytes are a tagged pointer, first 2 bits are the tag for class
+            std::uint64_t _state[2];
+        };
+        _gs_impl _impl;
 
     public:
-        german_string(const char *str, size_type size, string_class cls)
-        {
-            _state[0] = size;
-            if (_is_small())
-            {
-                std::memcpy(_get_maybe_small_ptr(), str, size);
-            }
-            else
-            {
-                if (cls == string_class::temporary)
-                {
-                    char *copied_str = new char[size];
-                    std::memcpy(copied_str, str, size);
-                    str = copied_str;
-                }
-                _state[1] = reinterpret_cast<std::uint64_t>(str) | _get_ptr_tag(cls);
-            }
-        }
-
-        german_string(const char* c_str, string_class cls = string_class::temporary)
-            : german_string(c_str, std::strlen(c_str), cls)
+        basic_german_string(const TAllocator& allocator = TAllocator()) noexcept(noexcept(TAllocator(allocator)))
+            : _impl(allocator)        
         {
         }
 
-        ~german_string()
+        basic_german_string(const char *str, size_type size, string_class cls, const TAllocator& allocator = TAllocator())
+            : _impl(str, size, cls, allocator)
         {
-            if (!_is_small())
-            {
-                std::uint64_t tag = _state[1] & PTR_TAG_MASK;
-                if (tag == _get_ptr_tag(string_class::temporary))
-                {
-                    delete[] _get_ptr();
-                }
-            }
         }
+
+        basic_german_string(const char* c_str, string_class cls = string_class::temporary)
+            : basic_german_string(c_str, std::strlen(c_str), cls)
+        {
+        }
+
+        basic_german_string(std::nullptr_t) = delete;
+
+        basic_german_string(const basic_german_string&) = default;
+        basic_german_string(basic_german_string&&) = default;
+        basic_german_string& operator=(const basic_german_string&) = default;
+        basic_german_string& operator=(basic_german_string&&) = default;
 
         string_class get_class() const
         {
-            if (_is_small())
-            {
-                return string_class::persistent;
-            }
-            else
-            {
-                std::uint64_t tag = (_state[1] & PTR_TAG_MASK) >> 62;
-                return static_cast<string_class>(tag);
-            }
+            return _impl._get_class();
         }
 
         size_type get_size() const
         {
-            return _get_size();
+            return _impl._get_size();
         }
 
         size_type length() const
         {
-            return _get_size();
+            return _impl._get_size();
         }
 
         std::string_view as_string_view() const
         {
-            return std::string_view(_get_maybe_small_ptr(), _get_size());
+            return std::string_view(_impl._get_maybe_small_ptr(), _impl._get_size());
         }
 
-        bool operator==(const german_string &other) const
+        bool operator==(const basic_german_string &other) const
         {
-            if (_state[0] != other._state[0])
-            {
-                return false;
-            }
-
-            if (_is_small())
-            {
-                return _state[1] == other._state[1];
-            }
-            return std::memcmp(_get_ptr(), other._get_ptr(), get_size()) == 0;
+            return _impl._equals(other._impl);
         }
 
-
+        bool empty() const
+        {
+            return _impl._get_size() == 0;
+        }
     };
+
+    using german_string = basic_german_string<>;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,9 +1,66 @@
+#include <random>
+#include <string>
+#include <vector>
+
+#include "german_string.h"
+
 #include <gtest/gtest.h>
 
+constexpr auto SMALL_KNOWN_STRING = "Hello, World!";
+constexpr auto LARGE_KNOWN_STRING = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+template <typename StringType>
+std::vector<StringType> generate_random_strings(size_t count, size_t min_length, size_t max_length, uint32_t seed)
+{
+    std::vector<StringType> strings;
+    strings.reserve(count);
+    std::string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?";
+    std::mt19937 generator(seed);
+    std::uniform_int_distribution<> length_distribution(min_length, max_length);
+    std::uniform_int_distribution<> char_distribution(0, alphabet.size() - 1);
+
+    constexpr float small_known_string_probability = 0.1f; // 10% chance to generate the small known string
+    constexpr float large_known_string_probability = 0.05f; // 5% chance to generate the large known string
+    std::uniform_real_distribution<> known_string_distribution(0.0f, 1.0f);
+
+    for (size_t i = 0; i < count; ++i)
+    {
+        // This is probably statistically wrong, but it works for the test
+        if (known_string_distribution(generator) < small_known_string_probability)
+        {
+            strings.emplace_back(SMALL_KNOWN_STRING);
+            continue;
+        }
+        else if (known_string_distribution(generator) < large_known_string_probability)
+        {
+            strings.emplace_back(LARGE_KNOWN_STRING);
+            continue;
+        }
+
+        std::string new_built_string;
+        size_t length = length_distribution(generator);
+        for (size_t j = 0; j < length; ++j)
+        {
+            new_built_string += alphabet[char_distribution(generator)];
+        }
+        strings.emplace_back(new_built_string.c_str());
+    }
+    return strings;
+}
+
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+TEST(GermanStrings, BasicCorrectness) {
+  size_t count = 100;
+  size_t min_length = 8;
+  size_t max_length = 240;
+  uint32_t seed = 42;
+  auto std_strings = generate_random_strings<std::string>(count, min_length, max_length, seed);
+  auto german_strings = generate_random_strings<gs::german_string>(count, min_length, max_length, seed);
+
+  for (size_t i = 0; i < count; ++i)
+  {
+      EXPECT_EQ(std_strings[i].length(), german_strings[i].length());
+      EXPECT_EQ(std_strings[i].compare(german_strings[i].as_string_view()), 0);
+      EXPECT_EQ(std_strings[i], german_strings[i].as_string_view());
+  }
 }
