3e7d7dcdcf0280296ece2a8f402fa0d0946a2085
Dmytro WSL <dmytroshynkar@gmail.com>
2025-08-12 01:47:48 +0200

    actually include new files

---
+++ triplets/x64-linux-libcxx.cmake
@@ -0,0 +1,10 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+# Use libc++ instead of libstdc++
+set(VCPKG_CXX_FLAGS "-stdlib=libc++")
+set(VCPKG_C_FLAGS "")
+set(VCPKG_LINKER_FLAGS "-stdlib=libc++ -lc++abi")
